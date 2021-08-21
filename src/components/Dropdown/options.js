de917a39f4d0f26bf1a14f2d8293dbf6c1171ca1
Francisco <fd07ass@gmail.com>
2021-08-21 16:38:28 -0300

    Provider and dimensions

--- src/components/Dropdown/options.js
+++ src/components/Dropdown/options.js
@@ -1,10 +1,78 @@
-import React from 'react';
+import React, { useRef, useState, useContext, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+import { useDimensions } from './dimensions';
+import { Context } from './Provider';
+
+let lastOptionId = 0;
 
 export function DropdownOption({ name, content: Content, backgroundHeight }) {
+    const idRef = useRef(++lastOptionId);
+    const id = idRef.current;
+
+    const [optionHook, optionDimensions] = useDimensions();
+    const [registered, setRegistered] = useState(false);
+
+    const {
+        registerOption,
+        updateOptionProps,
+        deleteOptionById,
+        setTargetId,
+        targetId
+    } = useContext(Context);
+
+    useEffect(() => {
+        if (!registered && optionDimensions) {
+            const WrappedContent = () => {
+                const contetRef = useRef();
+
+                useEffect(() => {
+                    const contentDimensions = contetRef.current.getBoundingClientRect();
+                    updateOptionProps(id, { contentDimensions })
+                }, [])
+
+                return (
+                    <div ref={contetRef}>
+                        <Content />
+                    </div>
+                )
+            };
+
+            registerOption({
+                id,
+                optionDimensions,
+                optionCenterX: optionDimensions.x + (optionDimensions.width / 2),
+                WrappedContent,
+                backgroundHeight
+            });
+
+            setRegistered(true)
+        } else if (registerOption && optionDimensions) {
+            updateOptionProps(id, {
+                optionDimensions,
+                optionCenterX: optionDimensions.x + (optionDimensions.width / 2),
+            });
+        }
+
+    }, [
+        registerOption,
+        id, 
+        registered,
+        optionDimensions,
+        updateOptionProps, 
+        deleteOptionById, 
+        backgroundHeight
+    ]);
+
+
+
     return (
-        <button 
-            className='dropdown-option'>
-                {name}
-        </button>
+        <motion.button
+            className='dropdown-option'
+            ref={optionHook}
+        >
+
+            {name}
+        </motion.button>
     )
 }
